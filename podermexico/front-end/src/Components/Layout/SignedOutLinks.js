06141cff5fa7d6ce464d6d503d77259f7a52286f
kainmaligno <s.e.jugr@gmail.com>
2018-09-21 02:21:02 -0500

    navbar y botones

---
+++ podermexico/front-end/src/Components/Layout/SignedOutLinks.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+
+const SignedOutLinks = ()=>{
+return (
+ <ul className='right'>
+  <li><NavLink to='/'>Registro</NavLink></li>
+  <li><NavLink to='/'>Entrar</NavLink></li>
+  
+ </ul>
+)
+}
+
+export default SignedOutLinks
--- podermexico/front-end/src/routes.js
+++ podermexico/front-end/src/routes.js
@@ -6,12 +6,13 @@ import Signup from './Components/Auth/Signup';
 import Login from './Components/Auth/Login';
 import Private from './Components/Auth/Private';
 import Header from './Components/Header';
-
+import Home from './Components/Home';
 
 
     export default () => <Router>
       <div className='container'>
-        <Route exact path = '/' component = {Header}/>
+       <Route exact path = '/home' component = {Home}/>
+        {/* <Route exact path = '/' component = {Header}/> */}
         <Route  path = '/signup'  component = {Signup}/>
         <Route  path = '/private' component = {Private}/>
         <Route  path = '/login' component = {Login}/>
