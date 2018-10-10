7eae2c2d6474ee231f876a669f04152e9ee7c4d6
kainmaligno <s.e.jugr@gmail.com>
2018-10-09 22:18:01 -0500

    go heroku

--- podermexico/app.js
+++ podermexico/app.js
@@ -6,7 +6,7 @@ const express      = require('express');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
+const favicon     =require('express-favicon');
 const session    = require("express-session");
 const MongoStore = require('connect-mongo')(session);
 const flash      = require("connect-flash");
@@ -36,7 +36,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
       
-
+app.use(favicon(__dirname+'front-end/build/favicon.ico'))
+app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, 'front-end/build')));
 
 // Enable authentication using session + passport
--- podermexico/routes/index.js
+++ podermexico/routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router  = express.Router();
-
+const path = require('path')
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.send('index');
+  res.sendFile(path.join(__dirname,'front-end/build','index.html'));
 });
 
 module.exports = router;
